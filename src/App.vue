86c03d5763b56ffc10ce2ccefa0bd5694c75c158
Andry Pebrianto <andrypeb227@gmail.com>
2024-02-23 23:33:34 +0700

    installing pinia & making a store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
+  <header>
+    <h1>Pinia Task</h1>
+  </header>
+
   <main>
-    <!-- heading -->
-    <header>
-      <h1>Pinia Task</h1>
-    </header>
+    <p>Saya Adalah Bambang</p>
   </main>
 </template>
+
+<style scoped>
+header h1 {
+  padding: 10px;
+  background-color: yellow;
+  text-align: center;
+  color: black;
+}
+
+main {
+  margin-top: 20px;
+  padding: 20px;
+}
+</style>
